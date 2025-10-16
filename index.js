41761ed55ff76a1e4050e84613ff286cb33143c0
krismos64 <c.mostefaoui@yahoo.fr>
2025-10-16 12:58:17 +0200

    train querySelector and addEventListener

---
+++ index.js
@@ -0,0 +1,27 @@
+const questionContainer = document.querySelector(".click-event");
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const response = document.querySelector("p");
+
+questionContainer.addEventListener("click", () => {
+  questionContainer.classList.toggle("question-clicked");
+});
+
+btn1.addEventListener("click", () => {
+  response.classList.add("show-response");
+  response.style.background = "green";
+});
+
+btn2.addEventListener("click", () => {
+  response.classList.add("show-response");
+  response.style.background = "red";
+});
+
+//___________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+
+const mousemove = document.querySelector(".mousemove");
+
+window.addEventListener("mousemove", (e) => {
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
+});
